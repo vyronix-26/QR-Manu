90f53b28bda90c85b90a6beb8d8b8d0e0d96be3d
akef <akefali43@gmail.com>
2026-07-21 19:20:55 +0300

    Fix image paths using public folder

--- src/app/data/menuData.ts
+++ src/app/data/menuData.ts
@@ -7,42 +7,42 @@ export const menuItems: MenuItem[] = [
     price: 45, 
     category: "main_dishes", 
     description: "شريحتين من لحم الأنجوس المشوي مع الجبن السائل، الخس، والطماطم يقدم مع البطاطس",
-    image: "/src/img/hamburguesa-cerdo-delicia-gourmet-adornada-tocino-queso-papas-fritas_795881-11284.jpg"
+    image: "/img/hamburguesa-cerdo-delicia-gourmet-adornada-tocino-queso-papas-fritas_795881-11284.jpg"
   },
   { id: "m2",
      name: "برجر دجاج مقرمش",
       price: 38, 
       category: "main_dishes", 
       description: "صدر دجاج مقرمش مع صلصة المايونيز الحارة والخس الطازج",
-      image: "/src/img/OIP.webp"
+      image: "/img/OIP.webp"
    },
   { id: "m3", 
     name: "بيتزا مارغريتا", 
     price: 40, 
     category: "main_dishes",
      description: "عجينة إيطالية تقليدية مع صلصة الطماطم الغنية وجبن الموزاريلا الفاخر",
-     image: "/src/img/OIP (1).webp"
+     image: "/img/OIP (1).webp"
      },
   { id: "m4", 
     name: "بيتزا دجاج باربكيو", 
     price: 48,
      category: "main_dishes", 
      description: "قطع دجاج مشوية، بصل، جبن موزاريلا، وصلصة الباربكيو المدخنة",
-     image: "/src/img/R.jfif"
+     image: "/img/R.jfif"
      },
   { id: "m5",
      name: "باستا الدجاج بالكريمة (ألفredo)",
       price: 42, 
       category: "main_dishes", 
       description: "معكرونة الفيتوتشيني مع قطع الدجاج والفطر في صلصة الكريمة البيضاء الغنية",
-    image: "/src/img/OIP (2).webp"
+    image: "/img/OIP (2).webp"
     },
   { id: "m6", 
     name: "وجبة شاورما عربي سوبر",
      price: 35,
       category: "main_dishes",
        description: "قطع شاورما دجاج متبلة في خبز صاج، تقطع وتقدم مع بطاطس ومقبلات وثومية",
-      image: "/src/img/OIP (3).webp"
+      image: "/img/OIP (3).webp"
       },
 
   // --- مقبلات وسلطات (appetizers) ---
@@ -51,35 +51,35 @@ export const menuItems: MenuItem[] = [
      price: 18,
       category: "appetizers", 
       description: "أصابع بطاطس مقرمشة مغطاة بصلصة الجبن الدافئة",
-     image: "/src/img/3653556429.jpg"
+     image: "/img/3653556429.jpg"
     },
   { id: "a2",
      name: "أصابع الموزاريلا المقرمشة",
       price: 22,
        category: "appetizers",
         description: "4 قطع من أصابع جبن الموزاريلا المقلية تقدم مع صلصة المارينارا",
-      image: "/src/img/555.jpg"
+      image: "/img/555.jpg"
       },
   { id: "a3",
      name: "حلقات البصل المقرمشة", 
      price: 15, 
      category: "appetizers",
       description: "حلقات بصل ذهبية مقلية تقدم مع صلصة الباربكيو",
-    image: "/src/img/OIP (6).webp"
+    image: "/img/OIP (6).webp"
     },
   { id: "a4",
      name: "سلطة سيزر بالدجاج",
       price: 25,
        category: "appetizers",
         description: "خس روماني، قطع دجاج مشوية، خبز محمص، مغطاة بصلصة السيزر وجبن البارميزان",
-      image: "/src/img/OIP (7).webp"
+      image: "/img/OIP (7).webp"
       },
   { id: "a5",
      name: "سلطة يونانية",
       price: 20,
        category: "appetizers",
         description: "طماطم، خيار، بصل، زيتون أسود، وجبن الفيتا مع زيت الزيتون والزعتر",
-      image: "/src/img/OIP (8).webp"
+      image: "/img/OIP (8).webp"
       },
 
   // --- مشروبات (drinks) ---
@@ -88,35 +88,35 @@ export const menuItems: MenuItem[] = [
       price: 15,
        category: "drinks",
         description: "عصير برتقال طبيعي 100% معصور طازجاً",
-      image: "/src/img/pngtree-orange-juice-fruit-oranges-tangerines-poster-image_1033353.jpg"
+      image: "/img/pngtree-orange-juice-fruit-oranges-tangerines-poster-image_1033353.jpg"
       },
   { id: "dr2",
      name: "ليمون بالنعناع بارد",
       price: 14,
        category: "drinks",
         description: "مزيج منعش من الليمون الحامض والنعناع الطازج مع الثلج",
-      image: "/src/img/OIP (4).webp"
+      image: "/img/OIP (4).webp"
       },
   { id: "dr3",
      name: "ميلك شيك شوكولاتة",
       price: 18,
        category: "drinks",
         description: "مخفوق الحليب البارد مع آيس كريم الشوكولاتة والكريمة المخفوقة",
-      image: "/src/img/OIP (5).jpg"
+      image: "/img/OIP (5).jpg"
       },
   { id: "dr4",
      name: "مشروبات غازية",
       price: 8,
        category: "drinks",
         description: "كوكا كولا، سبرايت، فانتا (حسب اختيارك)",
-      image: "/src/img/R (1).jfif"
+      image: "/img/R (1).jfif"
       },
   { id: "dr5",
      name: "مياه معدنية",
       price: 5,
        category: "drinks",
         description: "زجاجة مياه نقية مبردة",
-      image: "/src/img/OIP (5).webp"
+      image: "/img/OIP (5).webp"
       },
 
   // --- حلويات (desserts) ---
@@ -125,27 +125,27 @@ export const menuItems: MenuItem[] = [
       price: 25,
        category: "desserts", 
        description: "كعكة الشوكولاتة الدافئة المحشوة بالشوكولاتة السائلة تقدم مع كرة آيس كريم فانيليا",
-      image: "/src/img/OIP (9).webp"
+      image: "/img/OIP (9).webp"
       },
   { id: "de2",
      name: "وافل نوتيلا",
       price: 24,
        category: "desserts",
         description: "قطع الوافل المقرمشة المغطاة بشوكولاتة النوتيلا الغنية وقطع الفراولة",
-      image: "/src/img/OIP (10).webp"
+      image: "/img/OIP (10).webp"
       },
   { id: "de3",
      name: "سلطة فواكه بالآيس كريم",
       price: 20,
        category: "desserts",
         description: "تشكيلة من الفواكه الموسمية الطازجة مع مغرفة من الآيس كريم",
-      image: "/src/img/OIP (11).webp"
+      image: "/img/OIP (11).webp"
       },
   { id: "de4",
      name: "تشيز كيك الفراولة",
       price: 22,
        category: "desserts",
         description: "طبقة غنية من التشيز كيك الكريمي فوق طبقة البسكويت المقرمشة مع صوص الفراولة",
-      image: "/src/img/R (2).jfif"
+      image: "/img/R (2).jfif"
       }
 ];
